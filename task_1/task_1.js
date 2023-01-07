7c0b1d80abb4615cf74c172dcc092a242d5edbbc
helga-umrikh <zetaumr@gmail.com>
2023-01-07 23:38:39 +0300

    added in alert space between string and number

--- task_1/task_1.js
+++ task_1/task_1.js
@@ -2,4 +2,4 @@ dollar = prompt('Please enter your dollar amount');
 
 dolRub_exch_rate = dollar * 72;
 
-alert('Your ending amount is:' + dolRub_exch_rate);
+alert('Your ending amount is: ' + dolRub_exch_rate);
