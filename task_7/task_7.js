79a785698d2c5d8ecdcc40fab529e2e4968828df
helga-umrikh <zetaumr@gmail.com>
2023-01-12 02:30:46 +0300

    added zero value

--- task_7/task_7.js
+++ task_7/task_7.js
@@ -1,18 +1,21 @@
-const arr =  [1, null, 6, 'f', 5, 8];
+const arr =  [0, 1, null, 6, 'f', 5, 8];
 const arrOnlyNumbers = arr.filter( element => typeof(element) === 'number');
 
 let odd = 0;
 let even = 0;
+let zero = 0;
 let result;
 
 for(let i = 0; arrOnlyNumbers.length > i; i++) {
-    if (arrOnlyNumbers[i] % 2 === 0) {
+    if (arrOnlyNumbers[i] === 0) {
+    zero++;
+    } else if(arrOnlyNumbers[i] % 2 === 0) {
     even++;
     } else {
     odd++;
     }
 }
 
-result = 'even numbers: ' + even + ' and odd numbers: ' + odd;
+result = 'even numbers: ' + even + ', odd numbers: ' + odd + ', zero symbols: ' + zero;
 
 console.log(result)
