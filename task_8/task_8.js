ce854011ef49478ad708f58e24b14caaeb88c817
helga-umrikh <zetaumr@gmail.com>
2023-01-12 03:18:22 +0300

    added task_8.js

---
+++ task_8/task_8.js
@@ -0,0 +1,9 @@
+let countMap = new Map([
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+    ]);
+
+    for (const [key, value] of countMap) {
+    console.log('Key - ' + `${key}` + ' value - ' + `${value}`);
+    }
